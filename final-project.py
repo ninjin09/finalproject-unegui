069de826b6a75ea12c52aee55cdac13c33e1d88e
ninjin09 <117459736+ninjin09@users.noreply.github.com>
2023-11-20 23:26:53 +0800

    Update final-project.py

--- final-project.py
+++ final-project.py
@@ -16,18 +16,18 @@ filtered_df = df[(df['price'] >= price_range[0]) & (df['price'] <= price_range[1
 
 show_description = st.sidebar.checkbox('Show Description', value=False)
 
-selection = st.sidebar.selectbox('Select a manufacturer', ('All', 'HP', 'Lenovo', 'Acer', 'Asus', 'Dell', 
-                                                           'Apple', 'Gateway', 'other', 'MSI', 'Samsung',
-                                                           'Evoo', 'Sony', 'Microsoft Surface'))
+# selection = st.sidebar.selectbox('Select a manufacturer', ('All', 'HP', 'Lenovo', 'Acer', 'Asus', 'Dell', 
+#                                                            'Apple', 'Gateway', 'other', 'MSI', 'Samsung',
+#                                                            'Evoo', 'Sony', 'Microsoft Surface'))
 
-if selection == "All":
-    filtered_df1 = filtered_df
-else:
-    filtered_df1 = filtered_df[filtered_df['manufacturer'] == selection]
+# if selection == "All":
+#     filtered_df1 = filtered_df
+# else:
+#     filtered_df1 = filtered_df[filtered_df['manufacturer'] == selection]
     
     
 if not show_description:
-    st.dataframe(filtered_df1.drop('description', axis=1))
+    st.dataframe(filtered_df.drop('description', axis=1))
 else:
-    st.dataframe(filtered_df1)
+    st.dataframe(filtered_df)
 
