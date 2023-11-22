6f02fc58869577fe9e972d09a344c51bd3e77fe3
ninjin09 <117459736+ninjin09@users.noreply.github.com>
2023-11-23 02:33:36 +0800

    Update final-project.py

--- final-project.py
+++ final-project.py
@@ -7,12 +7,13 @@ df = pd.read_csv('finaldf.csv')
 
 st.sidebar.title('Filter Options')
 
-price_range = st.sidebar.slider('Select Price Range', min_value=df['price'].min(), max_value=df['price'].max(), 
+price_range = st.sidebar.slider('Select Price Range', step=1000, min_value=df['price'].min(), max_value=df['price'].max(), 
                                 value=(df['price'].min(), df['price'].max()))
 
-selection = st.sidebar.selectbox('select',("All", "HP", "Lenovo", "Acer", "Asus", "Dell", 
+selection = st.sidebar.selectbox('Select a Manufacturer',("All", "HP", "Lenovo", "Acer", "Asus", "Dell", 
                                                            "Apple", "Gateway", "other", "MSI", "Samsung",
                                                            "Evoo", "Sony", "Microsoft Surface"))
+used = st.sidebar.selectbox('Sort by Condition',("All","New","Used"))
 
 if selection == "All":
     filtered_df = df
@@ -21,20 +22,25 @@ else:
 
 filtered_df1 = filtered_df[(filtered_df['price'] >= price_range[0]) & (filtered_df['price'] <= price_range[1])]
 
+if used == "All":
+    filtered_df2 = filtered_df1
+else:
+    filtered_df2 = filtered_df1[filtered_df1['condition'] == used]
+
 show_description = st.sidebar.checkbox('Show Description', value=False)
 
 if not show_description:
-    filtered_df2 = filtered_df1.drop('description', axis=1)
-else:
-    filtered_df2 = filtered_df1
-    
-show_nans = st.sidebar.checkbox('Hide Nan Values', value=False)
-if show_nans:
-    filtered_df3 = filtered_df2.dropna()
+    filtered_df3 = filtered_df2.drop('description', axis=1)
 else:
     filtered_df3 = filtered_df2
     
-st.dataframe(filtered_df3, width=3000)
+show_nans = st.sidebar.checkbox('Hide Nan Values', value=False)
+if show_nans:
+    filtered_df4 = filtered_df3.dropna()
+else:
+    filtered_df4 = filtered_df3
+    
+st.dataframe(filtered_df4, width=3000)
 
 
 st.write(f'### Bar Chart of Avg Price per Manufacturer')
